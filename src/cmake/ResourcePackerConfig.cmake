de3de5ad5df7e634ee287f1a9a1697c40623d9cf
Stephen Seo <seo.disparate@gmail.com>
2014-07-19 15:49:04 -0400

    Modified config for windows support

--- src/cmake/ResourcePackerConfig.cmake
+++ src/cmake/ResourcePackerConfig.cmake
@@ -3,10 +3,17 @@ get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 include(${SELF_DIR}/ResourcePackerTargets.cmake)
 
+if(WIN32)
+set(ResourcePacker_INCLUDE_DIR ${SELF_DIR}/../../../include)
+set(ResourcePacker_INCLUDE_DIRS ${SELF_DIR}/../../../include)
+set(ResourcePacker_LIBRARY ${SELF_DIR}/../../../bin/libResourcePacker.dll)
+set(ResourcePacker_LIBRARIES ${SELF_DIR}/../../../bin/libResourcePacker.dll)
+else()
 set(ResourcePacker_INCLUDE_DIR ${SELF_DIR}/../../../include)
 set(ResourcePacker_INCLUDE_DIRS ${SELF_DIR}/../../../include)
 set(ResourcePacker_LIBRARY ${SELF_DIR}/../../../lib/libResourcePacker.so)
 set(ResourcePacker_LIBRARIES ${SELF_DIR}/../../../lib/libResourcePacker.so)
+endif()
 
 get_filename_component(ResourcePacker_INCLUDE_DIR ${ResourcePacker_INCLUDE_DIR} ABSOLUTE)
 get_filename_component(ResourcePacker_INCLUDE_DIRS ${ResourcePacker_INCLUDE_DIRS} ABSOLUTE)
