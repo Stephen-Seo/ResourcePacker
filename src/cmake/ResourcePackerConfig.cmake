7ab76648e589edf35572784e30dbc50e769f2a0b
Stephen Seo <seo.disparate@gmail.com>
2014-07-19 12:09:07 -0400

    Set paths to absolute paths, added found message

--- src/cmake/ResourcePackerConfig.cmake
+++ src/cmake/ResourcePackerConfig.cmake
@@ -7,3 +7,10 @@ set(ResourcePacker_INCLUDE_DIR ${SELF_DIR}/../../../include)
 set(ResourcePacker_INCLUDE_DIRS ${SELF_DIR}/../../../include)
 set(ResourcePacker_LIBRARY ${SELF_DIR}/../../../lib/libResourcePacker.so)
 set(ResourcePacker_LIBRARIES ${SELF_DIR}/../../../lib/libResourcePacker.so)
+
+get_filename_component(ResourcePacker_INCLUDE_DIR ${ResourcePacker_INCLUDE_DIR} ABSOLUTE)
+get_filename_component(ResourcePacker_INCLUDE_DIRS ${ResourcePacker_INCLUDE_DIRS} ABSOLUTE)
+get_filename_component(ResourcePacker_LIBRARY ${ResourcePacker_LIBRARY} ABSOLUTE)
+get_filename_component(ResourcePacker_LIBRARIES ${ResourcePacker_LIBRARIES} ABSOLUTE)
+
+message(STATUS "Found ResourcePacker at ${ResourcePacker_INCLUDE_DIR}")
