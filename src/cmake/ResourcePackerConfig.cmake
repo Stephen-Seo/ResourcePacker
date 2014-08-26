fda6d1fe484397c6c66ddde718486b042af10b5f
Stephen Seo <seo.disparate@gmail.com>
2014-08-26 02:53:29 -0400

    Fixed lib path resolution in config

--- src/cmake/ResourcePackerConfig.cmake
+++ src/cmake/ResourcePackerConfig.cmake
@@ -1,23 +1,18 @@
 
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+find_library(ResourcePacker_LIBRARY ResourcePacker
+    PATHS @CMAKE_INSTALL_PREFIX@/lib
+    NO_DEFAULT_PATH
+)
+find_library(ResourcePacker_LIBRARIES ResourcePacker
+    PATHS @CMAKE_INSTALL_PREFIX@/lib
+    NO_DEFAULT_PATH
+)
 
-include(${SELF_DIR}/ResourcePackerTargets.cmake)
-
-if(WIN32)
-set(ResourcePacker_INCLUDE_DIR ${SELF_DIR}/../../../include)
-set(ResourcePacker_INCLUDE_DIRS ${SELF_DIR}/../../../include)
-set(ResourcePacker_LIBRARY ${SELF_DIR}/../../../bin/libResourcePacker.dll)
-set(ResourcePacker_LIBRARIES ${SELF_DIR}/../../../bin/libResourcePacker.dll)
-else()
-set(ResourcePacker_INCLUDE_DIR ${SELF_DIR}/../../../include)
-set(ResourcePacker_INCLUDE_DIRS ${SELF_DIR}/../../../include)
-set(ResourcePacker_LIBRARY ${SELF_DIR}/../../../lib/libResourcePacker.so)
-set(ResourcePacker_LIBRARIES ${SELF_DIR}/../../../lib/libResourcePacker.so)
-endif()
-
-get_filename_component(ResourcePacker_INCLUDE_DIR ${ResourcePacker_INCLUDE_DIR} ABSOLUTE)
-get_filename_component(ResourcePacker_INCLUDE_DIRS ${ResourcePacker_INCLUDE_DIRS} ABSOLUTE)
-get_filename_component(ResourcePacker_LIBRARY ${ResourcePacker_LIBRARY} ABSOLUTE)
-get_filename_component(ResourcePacker_LIBRARIES ${ResourcePacker_LIBRARIES} ABSOLUTE)
-
-message(STATUS "Found ResourcePacker at ${ResourcePacker_INCLUDE_DIR}")
+find_path(ResourcePacker_INCLUDE_DIR ResourcePacker.hpp
+    PATHS @CMAKE_INSTALL_PREFIX@/include
+    NO_DEFAULT_PATH
+)
+find_path(ResourcePacker_INCLUDE_DIRS ResourcePacker.hpp
+    PATHS @CMAKE_INSTALL_PREFIX@/include
+    NO_DEFAULT_PATH
+)
