32ef4e04759763c18e1f5f0fc87632227edfdb5c
Stephen Seo <seo.disparate@gmail.com>
2014-11-29 16:10:04 -0500

    Fixed generation of ResourcePackerConfig.cmake

--- src/cmake/ResourcePackerConfig.cmake
+++ src/cmake/ResourcePackerConfig.cmake
@@ -1,18 +1,18 @@
 
 find_library(ResourcePacker_LIBRARY ResourcePacker
-    PATHS @CMAKE_INSTALL_PREFIX@/lib
+    PATHS "@CMAKE_INSTALL_PREFIX@/lib"
     NO_DEFAULT_PATH
 )
 find_library(ResourcePacker_LIBRARIES ResourcePacker
-    PATHS @CMAKE_INSTALL_PREFIX@/lib
+    PATHS "@CMAKE_INSTALL_PREFIX@/lib"
     NO_DEFAULT_PATH
 )
 
 find_path(ResourcePacker_INCLUDE_DIR ResourcePacker.hpp
-    PATHS @CMAKE_INSTALL_PREFIX@/include
+    PATHS "@CMAKE_INSTALL_PREFIX@/include"
     NO_DEFAULT_PATH
 )
 find_path(ResourcePacker_INCLUDE_DIRS ResourcePacker.hpp
-    PATHS @CMAKE_INSTALL_PREFIX@/include
+    PATHS "@CMAKE_INSTALL_PREFIX@/include"
     NO_DEFAULT_PATH
 )
